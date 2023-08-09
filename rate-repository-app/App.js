9f163dba998cafad0d3e70dcdce25b9c5a1baedd
Bigya <bigyabajracharya@gmail.com>
2023-08-08 23:51:03 -0400

    Integrate eslint scripts

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <Text style={styles.header}>HI</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    color:'purple',
+    fontSize:'19px',
+  }
 });
