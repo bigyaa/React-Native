9828d9a7a291373fd4d327e83607702cc3a8444d
Bigya <bigyabajracharya@gmail.com>
2023-08-12 17:01:22 -0400

    Initialize the src directory

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,25 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <Text style={styles.header}>HI</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header:{
-    color:'purple',
-    fontSize:'19px',
-  }
-});
+export default App;
