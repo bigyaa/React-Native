66a94ea2cf1056c52a96b2f002beead58d998a05
Bigya <bigyabajracharya@gmail.com>
2023-08-20 23:54:32 -0400

    Add styles to welcome page

--- rn_jobs/components/home/welcome/Welcome.jsx
+++ rn_jobs/components/home/welcome/Welcome.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Image, FlatList, TouchableOpacity } from 'react-native'
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+import { useRouter } from 'expo-router'
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hi</Text>
+        <Text style={styles.welcomeMessage}>Welcome!!</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
   )
 }
