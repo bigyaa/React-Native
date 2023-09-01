a4625dd7a4ef1686ad9e719d6385b319958ace8c
Bigya <bigyabajracharya@gmail.com>
2023-08-31 23:00:24 -0400

    Format welcome page code

--- rn_jobs/components/home/welcome/Welcome.jsx
+++ rn_jobs/components/home/welcome/Welcome.jsx
@@ -18,29 +18,39 @@ const Welcome = () => {
         <Text style={styles.welcomeMessage}>Welcome!!</Text>
       </View>
 
-      <View style={styles.searchContainer}>
-        <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} value="" onChange={() => { }} placeholder='What are you looking for?' />
+      <View
+        style={styles.searchContainer}>
+        <View
+          style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput} value="" onChange={() => { }} placeholder='What are you looking for?' />
         </View>
 
-        <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+        <TouchableOpacity
+          style={styles.searchBtn} onPress={() => { }}>
           <Image source={icons.search}
             resizeMode='contain'
             style={styles.searchBtnImage} />
         </TouchableOpacity>
       </View>
 
-      <View style={styles.tabsContainer}>
-        <FlatList data={jobTypes} renderItem={({ item }) =>
-        (
-          <TouchableOpacity style={styles.tab(activeJobType, item)}
-            onPress={() => {
-              setActiveJobType(item);
-              router.push('/search/${item}');
-            }} >
-            <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
-          </TouchableOpacity>
-        )}
+      <View
+        style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) =>
+          (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push('/search/${item}');
+              }} >
+              <Text style={styles.tabText(activeJobType, item)}>
+                {item}
+              </Text>
+            </TouchableOpacity>
+          )}
           keyExtractor={(item) => item}
           contentContainerStyle={{ columnGap: SIZES.small }}
           horizontal
